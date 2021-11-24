2fc45b9f60e3884ce92ae765465b4ec2c244e69b
Mayuri2001 <patidarmayuri27@gmail.com>
2021-11-25 00:13:53 +0530

    command prompt

--- index.py
+++ index.py
@@ -1,5 +1,53 @@
-hello world 
-bye world
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import os
 
-mayuri patidar
-palak mehta
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+
+engine.setProperty('voices',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source,timeout=5,phrase_time_limit=5)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        speak("Say that again please...")
+        return "none"
+    return query 
+def wish():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour>0 and hour<12:
+        speak("Good Morning")
+    elif hour>12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("I am Jarvis , how can i help you")
+
+if __name__=="__main__":
+    wish()
+    # while True:
+    if 1:
+        query = takecommand().lower()
+
+        if "open notepad" in query:
+            npath = "C:\\WINDOWS\\system32\\notepad.exe"
+            os.startfile(npath)
+        elif "open command prompt" in query:
+            cpath="C:\\WINDOWS\\system32\\cmd.exe"
+            os.startfile(cpath)
+    # takecommand()
+    # speak("Hello there, What can I do for you")

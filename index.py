2c4ebd7f85c65bff205591f4e687cbaafe3567bc
Mayuri2001 <patidarmayuri27@gmail.com>
2021-11-27 18:00:59 +0530

    screenshot

--- index.py
+++ index.py
@@ -12,14 +12,15 @@ import wikipedia
 import webbrowser
 import pywhatkit as kit
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
 from email import encoders
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 import sys
 import pyjokes
 import pyautogui
 import email_to
+import instaloader
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -35,7 +36,7 @@ def takecommand():
     with sr.Microphone() as source:
         print("listening...")
         r.pause_threshold = 1
-        audio = r.listen(source,timeout=5,phrase_time_limit=5)
+        audio = r.listen(source,timeout=0,phrase_time_limit=5)
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
@@ -171,7 +172,7 @@ if __name__=="__main__":
         #         print(e)
         #         speak("sorry sir, i am not able to send this mail to mayuri")
 
-        elif "no thanks" in query:
+        elif "you can go to sleep" in query:
             speak("thanks for using me sir,have a good day")
             sys.exit()
 
@@ -179,7 +180,7 @@ if __name__=="__main__":
             speak("okay sir, closing notepad")
             os.system("taskkill /f /im notepad.exe")
 
-        elif "set alarm" in query:
+        elif "set an alarm" in query:
             nn=int(datetime.datetime.now().hour)
             if nn==22:
                 music_dir='C:\\songs'
@@ -194,7 +195,7 @@ if __name__=="__main__":
             os.system("shutdown /r /t s")
         elif "sleep the system" in query:
             os.system("rundl132.exe powrprof dil,SetSuspendState 0,1,0")
-        elif "switch the window" in query:
+        elif "change the window" in query:
             pyautogui.keyDown("alt")
             pyautogui.press("tab")
             time.sleep(1)
@@ -203,7 +204,7 @@ if __name__=="__main__":
         elif "tell me the news" in query:
             speak("please wait sir, fetching the latest news")
             news()
-            
+
         elif "switch the window" in query:
             pyautogui.keyDown("alt")
             pyautogui.press('tab')
@@ -259,7 +260,7 @@ if __name__=="__main__":
             else:
                 email = "patidarmayuri27@gmail.com"
                 password = "k_meenu_11"
-                send_to_email = "palakmehta030@gmail.com"
+                send_to_email = "palakmehta03@gmail.com"
                 message = query 
 
                 server = smtplib.SMTP('smtp.gmail.com',587)
@@ -268,3 +269,42 @@ if __name__=="__main__":
                 server.sendmail(email,send_to_email,message)
                 server.quit()
                 speak("email has been sent")
+
+        elif "where i am" in query or "where we are" in query:
+                speak("wait sir, let me check")
+                try:
+                    ipAdd = requests.get('https://api.ipify.org').text
+                    print(ipAdd)
+                    url = 'https://get.geojs.io/v1/ip/geo/'+ipAdd+'json'
+                    geo_requests = requests.get(url)
+                    geo_data = geo_requests.json()
+
+                    city = geo_data['city']
+
+                    country = geo_data['country']
+
+                    speak(f"sir i am not sure, but i think we are in {city} city of {country}")
+                except Exception as e:
+                    speak("sorry sir , due to network issues i am not able to find where we are ")
+                    pass 
+
+        elif "instagram profile" in query or "profile on instagram" in query:
+                speak("sir please enter the user name correctly")
+                name = input("Enter the username here:")
+                webbrowser.open(f"www.instagram.com/{name}")
+                speak("sir would you like to download profile picture of this account.")
+                condition = takecommand().lower()
+                if "yes" in condition:
+                    mod = instaloader.instaloader()
+                    mod.download_profile(name,profile_pic_only=True)
+                    speak("i am done sir, profile picture is saved in our main folder") 
+                else:
+                    pass
+        elif "take screenshot" in query or "take a screenshot" in query:
+            speak("sir,please tell me the name for this screenshot file")
+            name = takecommand().lower()
+            speak("please sir hold the screen for few seconds, i am taking screenshot")
+            time.sleep(3)
+            img = pyautogui.screenshot()
+            img.save(f"{name}.png")
+            speak("i am done sir, the screenshot is saved in our main folder");

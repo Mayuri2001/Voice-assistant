1fafefe45bc0e29da62a7563b9f0f9c498b8abb0
Mayuri2001 <patidarmayuri27@gmail.com>
2021-11-28 22:32:49 +0530

    gui

--- index.py
+++ index.py
@@ -37,6 +37,11 @@ voices = engine.getProperty('voices')
 
 engine.setProperty('voices',voices[0].id)
 
+engine.say('Bolne lagi. Mera yasu yasu, mera yasu yasu')
+
+
+
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -97,12 +102,36 @@ def pdf_reader():
     text=page.extractText()
     speak(text)
 
-def takecommand():
+# def takecommand():
+#         r = sr.Recognizer()
+#         with sr.Microphone() as source:
+#             print("listening...")
+#             r.pause_threshold = 1
+#             audio = r.listen(source,timeout=1,phrase_time_limit=5)
+#         try:
+#             print("Recognizing...")
+#             query = r.recognize_google(audio, language='en-in')
+#             print(f"user said: {query}")
+#         except Exception as e:
+#             speak("Say that again please...")
+#             return "none"
+#         # query = query.lower()
+#         return query
+
+
+class MainThread(QThread):
+    def __init__(self):
+        super(MainThread,self).__init__()
+
+    def run(self):
+        self.TaskExecution()
+    
+    def takecommand(self):
         r = sr.Recognizer()
         with sr.Microphone() as source:
             print("listening...")
             r.pause_threshold = 1
-            audio = r.listen(source,timeout=1,phrase_time_limit=5)
+            audio = r.listen(source,timeout=0,phrase_time_limit=5)
         try:
             print("Recognizing...")
             query = r.recognize_google(audio, language='en-in')
@@ -113,13 +142,6 @@ def takecommand():
         # query = query.lower()
         return query
 
-
-class MainThread(QThread):
-    def __init__(self):
-        super(MainThread,self).__init__()
-
-    def run(self):
-        self.TaskExecution()
         
     # def takecommand():
          
@@ -129,8 +151,9 @@ class MainThread(QThread):
             wish()
             while True:
             # if 1:
-                query = takecommand().lower()
+                query = self.takecommand().lower()
 
+                print (query)
                 if "open notepad" in query:
                     npath = "C:\\WINDOWS\\system32\\notepad.exe"
                     os.startfile(npath)
@@ -187,7 +210,7 @@ class MainThread(QThread):
 
                 elif "open google" in query:
                     speak("sir,what should i search on google")
-                    cm=takecommand().lower()
+                    cm=self.takecommand().lower()
                     webbrowser.open(f"{cm}") 
 
                 elif "send message" in query:
@@ -208,7 +231,7 @@ class MainThread(QThread):
                 #         print(e)
                 #         speak("sorry sir, i am not able to send this mail to mayuri")
 
-                elif "you can go to sleep" in query:
+                elif "you can go to sleep" or "okay,bye" in query:
                     speak("thanks for using me sir,have a good day")
                     sys.exit()
 
@@ -253,16 +276,16 @@ class MainThread(QThread):
 
                 elif "email to palak" in query:
                     speak("sir what should i say")
-                    query=takecommand().lower()
+                    query=self.takecommand().lower()
                     if "send a file" in query:
                         email='patidarmayuri27@gmail.com'
                         password='k_meenu_11'
                         send_to_email='palakmehta030@gmail.com'
                         speak("okay sir,what is the subject for this email")
-                        query=takecommand().lower()
+                        query=self.takecommand().lower()
                         subject=query
                         speak("and sir,what is the message for this email")
-                        query2=takecommand().lower()
+                        query2=self.takecommand().lower()
                         message=query2
                         speak("sir please enter the correct path of file in shell")
                         file_location=input("Please enter the path here")
@@ -329,7 +352,7 @@ class MainThread(QThread):
                         name = input("Enter the username here:")
                         webbrowser.open(f"www.instagram.com/{name}")
                         speak("sir would you like to download profile picture of this account.")
-                        condition = takecommand().lower()
+                        condition = self.takecommand().lower()
                         if "yes" in condition:
                             mod = instaloader.instaloader()
                             mod.download_profile(name,profile_pic_only=True)
@@ -338,7 +361,7 @@ class MainThread(QThread):
                             pass
                 elif "take screenshot" in query or "take a screenshot" in query:
                     speak("sir,please tell me the name for this screenshot file")
-                    name = takecommand().lower()
+                    name = self.takecommand().lower()
                     speak("please sir hold the screen for few seconds, i am taking screenshot")
                     time.sleep(3)
                     img = pyautogui.screenshot()
@@ -350,7 +373,7 @@ class MainThread(QThread):
 
                 elif "hide all files" in query or "hide this folder" in query or "visible for everyone" in query:
                     speak("sir please tell me u want to hide this folfer or make it visible for everyone")
-                    condition=takecommand().lower()
+                    condition=self.takecommand().lower()
                     if "hide" in condition:
                         os.system("attrib +h /s /d")
                         speak("sir,all the files in this folder are now hidden")
@@ -372,6 +395,7 @@ class Main(QMainWindow):
             self.ui.pushButton_2.clicked.connect(self.close)
 
         def startTask(self):
+            speak('')
             self.ui.movie = QtGui.QMovie("C:\\Users\\Hp\\Desktop\\project\\7LP8.gif")
             self.ui.jarvis.setMovie(self.ui.movie)
             self.ui.movie.start()
